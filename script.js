b864dee38902d7a28fadf0695310a0db455af3e6
PLAmark <gjejrgx7@gmail.com>
2026-04-02 02:03:34 +0700

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
+
+// КЛИК ПО ИГРЕ
+function selectGame(game) {
+  if (game === 'gta') {
+    tg.showAlert('Вы выбрали GTA 5 RP');
+
+    // пример перехода
+    window.location.href = 'gta.html';
+  }
+
+  if (game === 'br') {
+    tg.showAlert('Вы выбрали Black Russia');
+
+    window.location.href = 'br.html';
+  }
+}
+
+// КОПИРОВАНИЕ ССЫЛКИ
+function copyLink() {
+  const link = "https://t.me/your_bot?start=ref123"; // ЗАМЕНИ НА СВОЮ
+
+  navigator.clipboard.writeText(link)
+    .then(() => {
+      tg.showAlert("Ссылка скопирована!");
+    })
+    .catch(() => {
+      alert("Ошибка копирования");
+    });
+}
